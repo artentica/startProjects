b143d8622223e01d03266036882bee7d72d2880b
Riouallon Vincent <riouallonvincent@gmail.com>
2019-05-27 12:55:32 +0200

    chore: add reading file
    take the first label line

--- lauchApp.sh
+++ lauchApp.sh
@@ -105,6 +105,11 @@ multiselect () {
 INPUT=./projects.csv
 OLDIFS=$IFS
 IFS=","
-
+[ ! -f $INPUT ] &sed 1d $filename | while IFS=, read label default path
+do
+  echo "label : $label"
+  echo "default : $default"
+  echo "path : $path"
+done < $INPUT
 IFS=$OLDIFS
 # multiselect result "Option 1;Option 2;Option 3" "true;;true"
