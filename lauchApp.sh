6db87fdfe97399833c4f40da080d3392164c854e
Riouallon Vincent <riouallonvincent@gmail.com>
2019-05-28 12:55:32 +0200

    chore: read file without the first line and have the last one

--- lauchApp.sh
+++ lauchApp.sh
@@ -105,11 +105,20 @@ multiselect () {
 INPUT=./projects.csv
 OLDIFS=$IFS
 IFS=","
-[ ! -f $INPUT ] &sed 1d $filename | while IFS=, read label default path
-do
-  echo "label : $label"
-  echo "default : $default"
-  echo "path : $path"
-done < $INPUT
+# [ ! -f $INPUT ] &sed 1d $filename | while IFS=, read label default path
+# do
+#   echo "label : $label"
+#   echo "default : $default"
+#   echo "path : $path"
+# done < $INPUT
+{
+    read
+    while IFS=, read -r label default path
+    do
+      echo "label : $label"
+      echo "default : $default"
+      echo "path : $path"
+    done
+} < $INPUT
 IFS=$OLDIFS
 # multiselect result "Option 1;Option 2;Option 3" "true;;true"
